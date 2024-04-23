9ccd89c690ccdd2aef2278841c716b3ff7e64072
xonec <45141557+xonec@users.noreply.github.com>
2024-04-24 01:58:21 +0800

    Update dmpic.py

--- dmpic.py
+++ dmpic.py
@@ -23,15 +23,15 @@ from sklearn import cluster
 
 class DMPic:
     def __init__(self):
-
-    def GetCapture(self, stax, stay, endx, endy):
+    @staticmethod
+    def GetCapture( stax, stay, endx, endy):
         w = endx - stax
         h = endy - stay
         im = pyautogui.screenshot(region=(stax, stay, w, h))
         # im = cv2.cvtColor(np.array(im), cv2.COLOR_BGR2RGB)
         return np.array(im)
-
-    def FindPic(self, x1, y1, x2, y2, path, thd=0.9, type=1):
+    @staticmethod
+    def FindPic( x1, y1, x2, y2, path, thd=0.9, type=1):
         '''
         找图
         :param x1: 起点X
@@ -43,7 +43,7 @@ class DMPic:
         :param type: 默认1为灰度化找图，其他为彩色找图
         :return: 图片中心坐标
         '''
-        img = self.GetCapture(x1, y1, x2, y2)
+        img = GetCapture(x1, y1, x2, y2)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         template = cv2.imread(path)
         th, tw = template.shape[:2]
@@ -58,8 +58,8 @@ class DMPic:
             return -1, -1
         else:
             return maxLoc[0] + tw / 2 + x1, maxLoc[1] + th / 2 + y1
-
-    def FindPics(self, des):
+    @staticmethod
+    def FindPics(des):
         img = pyautogui.screenshot()
         img = np.array(img)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -77,16 +77,16 @@ class DMPic:
             else:
                 res.setdefault(key, (maxLoc[0] + tw / 2 + value[0], maxLoc[1] + th / 2 + value[1]))
         return res
-
-    def Hex_to_Rgb(self, hex):
+    @staticmethod
+    def Hex_to_Rgb(hex):
         '''
         十六进制转RGB
         :param hex: 十六进制颜色值
         :return: RGB
         '''
         return np.array(tuple(int(hex[i:i + 2], 16) for i in (0, 2, 4)))
-
-    def CmpColor(self, x, y, color, sim: float):
+    @staticmethod
+    def CmpColor(x, y, color, sim: float):
         '''
         比色
         :param x: X坐标
@@ -95,14 +95,14 @@ class DMPic:
         :param sim: 相似度(0-1对应二值化的255-0)，1为完全匹配
         :return: 真或假
         '''
-        img = self.GetCapture(x - 1, y - 1, x + 1, y + 1)
+        img = GetCapture(x - 1, y - 1, x + 1, y + 1)
         img = img[1][1]
-        color = self.Hex_to_Rgb(color)
+        color = Hex_to_Rgb(color)
         res = np.absolute(color - img)
         sim = int((1 - sim) * 255)
         return True if np.amax(res) <= sim else False
-
-    def FindColor(self, x1, y1, x2, y2, des, sim: float):
+    @staticmethod
+    def FindColor(x1, y1, x2, y2, des, sim: float):
         '''
         找色
         :param x1: 起点X
@@ -113,14 +113,14 @@ class DMPic:
         :param sim: 相似度(0-1对应二值化的255-0)，1为完全匹配
         :return:
         '''
-        img = self.GetCapture(x1, y1, x2, y2)
-        res = np.absolute(img - self.Hex_to_Rgb(des))
+        img = GetCapture(x1, y1, x2, y2)
+        res = np.absolute(img - Hex_to_Rgb(des))
         sim = int((1 - sim) * 255)
         res = np.argwhere(np.all(res <= sim, axis=2))
         res = res + (y1, x1)
         return res[:, [1, 0]]
-
-    def GetColorNum(self, x1, y1, x2, y2, des, sim: float):
+    @staticmethod
+    def GetColorNum(x1, y1, x2, y2, des, sim: float):
         '''
         获取颜色数量
         :param x1: 起点X
@@ -131,9 +131,9 @@ class DMPic:
         :param sim: 相似度(0-1对应二值化的255-0)，1为完全匹配
         :return:
         '''
-        return len(self.FindColor(x1, y1, x2, y2, des, sim))
-
-    def FindMultColor(self, stax, stay, endx, endy, des):
+        return len(FindColor(x1, y1, x2, y2, des, sim))
+    @staticmethod
+    def FindMultColor(stax, stay, endx, endy, des):
         '''
         多点找色
         :param stax:
@@ -183,8 +183,8 @@ class DMPic:
         if np.max(counts) == len(des.split(',')) - 1:
             return np.flipud(re)
         return np.array([-1, -1])
-
-    def FindPicEx(self, x1, y1, x2, y2, path, thd=0.9, MIN_MATCH_COUNT=8):
+    @staticmethod
+    def FindPicEx(x1, y1, x2, y2, path, thd=0.9, MIN_MATCH_COUNT=8):
         '''
         全分辨率找图
         :param x1:
@@ -199,7 +199,7 @@ class DMPic:
         thd = thd - 0.2
         template = cv2.imread(path, 0)  # queryImage
         # target = cv2.imread('target.jpg', 0)  # trainImage
-        target = self.GetCapture(x1, y1, x2, y2)
+        target = GetCapture(x1, y1, x2, y2)
         target = cv2.cvtColor(target, cv2.COLOR_BGR2GRAY)
         # Initiate SIFT detector创建sift检测器
         sift = cv2.xfeatures2d.SIFT_create()
@@ -232,8 +232,8 @@ class DMPic:
             return int(res[0][0]) + x1, int(res[0][1]) + y1
         else:
             return -1, -1
-
-    def _FilterRec(self, res, loc):
+    @staticmethod
+    def _FilterRec(res, loc):
         """ 对同一对象的多个框按位置聚类后，按置信度选最大的一个进行保留。
         :param res: 是 cv2.matchTemplate 返回值
         :param loc: 是 cv2.np.argwhere(res>threshold) 返回值
@@ -248,8 +248,8 @@ class DMPic:
             pt = argj[np.argmax(res[tuple(argi)])]
             pts.append(pt[::-1])
         return np.array(pts)
-
-    def FindMultPic(self, x1, y1, x2, y2, path, thd=0.8):
+    @staticmethod
+    def FindMultPic(x1, y1, x2, y2, path, thd=0.8):
         '''
         多目标找图
         :param x1:
@@ -260,19 +260,19 @@ class DMPic:
         :param thd: 相似度
         :return:
         '''
-        target = self.GetCapture(x1, y1, x2, y2)
+        target = GetCapture(x1, y1, x2, y2)
         target = cv2.cvtColor(target, cv2.COLOR_BGR2GRAY)
         template = cv2.imread(path, 0)
         w, h = template.shape[:2]
         res = cv2.matchTemplate(target, template, cv2.TM_CCOEFF_NORMED)
         loc = np.argwhere(res >= thd)
         if len(loc):
-            resc = self._FilterRec(res, loc)
+            resc = _FilterRec(res, loc)
             return resc + (h / 2 + x1, w / 2 + y1)
         else:
             return [[-1, -1]]
-
-    def FindPic_TM(self, x1, y1, x2, y2, path, thd=0.9):
+    @staticmethod
+    def FindPic_TM(x1, y1, x2, y2, path, thd=0.9):
         '''
         找透明图，透明色为黑色
         :param x1: 起点X
@@ -283,7 +283,7 @@ class DMPic:
         :param thd: 相似度
         :return: 图片中心坐标
         '''
-        img = self.GetCapture(x1, y1, x2, y2)
+        img = GetCapture(x1, y1, x2, y2)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         template = cv2.imread(path)
         template2 = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
@@ -295,8 +295,8 @@ class DMPic:
             return minLoc[0] + tw / 2 + x1, minLoc[1] + th / 2 + y1
         else:
             return -1, -1
-
-    def GetCaptre_TM(self, x1, y1, x2, y2, path, times=5):
+    @staticmethod
+    def GetCaptre_TM(x1, y1, x2, y2, path, times=5):
         '''
         动图变静态图片，改为静态图片后使用FindPic_TM进行找图
         :param x1:
@@ -326,8 +326,8 @@ class DMPic:
             captime = time.time() - st
         cv2.imwrite(path, a)
         return a
-
-    def StressShow(self, stax, stay, endx, endy, des, type=0):
+    @staticmethod
+    def StressShow(stax, stay, endx, endy, des, type=0):
         '''
         保留选中颜色，其他为黑色，相似度根据偏色调整
         :param stax:
@@ -341,10 +341,10 @@ class DMPic:
         # des = 'e81010-101010|f9ad08-000000'
         dess = des.split('|')
         des = [i[0:6] for i in dess]
-        des = [np.array(self.Hex_to_Rgb(d)) for d in des]
+        des = [np.array(Hex_to_Rgb(d)) for d in des]
         pds = [i[-6:] for i in dess]
         pds = tuple(tuple(int(item[i:i + 2]) for i in range(0, len(item), 2)) for item in pds)
-        img = self.GetCapture(stax, stay, endx, endy)
+        img = GetCapture(stax, stay, endx, endy)
         mask = np.zeros(img.shape[:2], dtype=np.bool_)
         for i, color in enumerate(des):
             mask += np.all(np.abs(img - color) <= pds[i], axis=-1)
@@ -354,15 +354,15 @@ class DMPic:
         img_converted = cv2.convertScaleAbs(new_img)
         img_converted = cv2.cvtColor(np.array(img_converted), cv2.COLOR_BGR2RGB)
         return img_converted
-
-    def SetDict(self, path):
+    @staticmethod
+    def SetDict(path):
         des = {}
         with open(path, 'r', encoding='GBK') as f:
             text = f.read()
             lines = text.splitlines()
             for line in lines:
                 parts = line.split("$")
-                # self.des.setdefault(parts[1],parts[0])
+                # des.setdefault(parts[1],parts[0])
                 bin_str = ''
                 for c in parts[0]:
                     byte = int(c, 16)
@@ -374,16 +374,16 @@ class DMPic:
                 arr = np.array([list(bin_str[i:i + 11]) for i in range(0, len(bin_str), 11)], dtype=np.float32)
                 arr = arr.transpose()
                 des.setdefault(parts[1], arr)
-        # print(self.des)
+        # print(des)
         return des
-
-    def FindString(self, x1, y1, x2, y2, strs, color, thd, DIict):
+    @staticmethod
+    def FindString(x1, y1, x2, y2, strs, color, thd, DIict):
         if strs not in DIict:
             print('字符串不存在')
             return -1, -1
         else:
             arr = DIict[strs]
-        img = self.StressShow(x1, y1, x2, y2, color, 1)
+        img = StressShow(x1, y1, x2, y2, color, 1)
         img = (img != 0).any(axis=2).astype(int)
         thresh = np.array(img, dtype=np.float32)
         result = cv2.matchTemplate(arr, thresh, cv2.TM_CCOEFF_NORMED)
@@ -394,14 +394,14 @@ class DMPic:
             return -1, -1
         else:
             return int(maxl[0] + h / 2 + x1), int(maxl[1] + w / 2 + y1)
-
-    def Ocr(self, x1, y1, x2, y2, des, thd, DIict):
+    @staticmethod
+    def Ocr(x1, y1, x2, y2, des, thd, DIict):
         dess = des.split('|')
         des = [i[0:6] for i in dess]
-        des = [np.array(self.Hex_to_Rgb(d)) for d in des]
+        des = [np.array(Hex_to_Rgb(d)) for d in des]
         pds = [i[-6:] for i in dess]
         pds = tuple(tuple(int(item[i:i + 2]) for i in range(0, len(item), 2)) for item in pds)
-        img = self.GetCapture(x1, y1, x2, y2)
+        img = GetCapture(x1, y1, x2, y2)
         mask = np.zeros(img.shape[:2], dtype=np.bool_)
         for i, color in enumerate(des):
             mask += np.all(np.abs(img - color) <= pds[i], axis=-1)
@@ -418,7 +418,7 @@ class DMPic:
             result = cv2.matchTemplate(value, img, cv2.TM_CCOEFF_NORMED)
             loc = np.argwhere(result >= thd)
             if len(loc):
-                resc = self._FilterRec(result, loc)
+                resc = _FilterRec(result, loc)
                 resc.astype(np.int64)
                 resc += np.array((h / 2 + x1, w / 2 + y1)).astype(np.int64)
                 resc = [(i[0], i[1]) for i in resc]
@@ -426,8 +426,8 @@ class DMPic:
             else:
                 res.setdefault(key, [(-1, -1)])
         return res
-
-    def getstr(self, original_data):
+    @staticmethod
+    def getstr(original_data):
         sorted_data = sorted(original_data.items(), key=lambda item: item[1][0][1])
         grouped_data = []
         for char, coord_list in sorted_data:
@@ -443,8 +443,8 @@ class DMPic:
                 if not added:
                     grouped_data.append([(char, coord) for coord in coord_list])
         return grouped_data
-
-    def OcrFix(self, input_dict, size=20):
+    @staticmethod
+    def OcrFix(input_dict, size=20):
         items = sorted(input_dict, key=lambda x: x[1][0])
         merged_dict = {}
         i = 0
@@ -463,16 +463,16 @@ class DMPic:
                 i += 1
             merged_dict[curr_key] = curr_value
         return merged_dict
-
-    def GetOcr(self, data):
+    @staticmethod
+    def GetOcr(data):
         res = {}
-        dat = self.getstr(data)
+        dat = getstr(data)
         for i in dat:
-            x = self.OcrFix(i)
+            x = OcrFix(i)
             res.update(x)
         return res
-
-    def FindPics_TM(self, des):
+    @staticmethod
+    def FindPics_TM(des):
         '''
         找多图
         :param des:
@@ -498,35 +498,3 @@ class DMPic:
                 break
         return res
 
-
-a = TuSe()
-time.sleep(3)
-node = {
-    'sb1': [6, 25, 1020, 849, './image/image0.bmp', 0.9],
-    'sb2': [6, 25, 1020, 849, './image/image1.bmp', 0.9],
-    'sb3': [6, 25, 1020, 849, './image/image10.bmp', 0.9],
-    'sb4': [6, 25, 1020, 849, './image/image11.bmp', 0.9],
-    'sb5': [6, 25, 1020, 849, './image/image13.bmp', 0.9],
-    'sb6': [6, 25, 1020, 849, './image/image15.bmp', 0.9],
-    'sb7': [6, 25, 1020, 849, './image/image17.bmp', 0.9],
-    'sb8': [6, 25, 1020, 849, './image/image19.bmp', 0.9],
-    'sb9': [6, 25, 1020, 849, './image/image25.bmp', 0.9],
-    'sb10': [6, 25, 1020, 849, './image/image27.bmp', 0.9],
-    'sb11': [6, 25, 1020, 849, './image/image29.bmp', 0.9]
-}
-
-while 1:
-    c = a.FindPics_TM(node)
-    print(c)
-    img = a.GetCapture(6, 25, 1020, 849)
-    height, width, _ = img.shape
-    center = (int(c[0]) - 6, int(c[1]) - 25)  # 圆心坐标
-    radius = 10  # 半径
-    color = (0, 0, 255)  # 颜色，这里是红色（BGR格式）
-    thickness = 2  # 线条粗细，这里是2个像素
-    cv2.circle(img, center, radius, color, thickness)
-    cv2.imshow("Image", img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-cv2.destroyAllWindows()
